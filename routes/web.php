90112476594e97af3a6833e8a2b95b06b3ec7264
iqbalmusyaffa <55908032+iqbalmusyaffa@users.noreply.github.com>
2023-05-07 11:04:52 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RouteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,31 @@ Route::get('/', function () {
 Route::get('/routing', function(){
     return view('routing');
 });
+
+Route::get('/basic_routing', function() {
+    return 'Hello World';
+});
+
+Route::view('/view_route', 'view_route');
+Route::view('/view_route', 'view_route', ['name' => 'Iqbal']);
+
+Route::get('/controller_route',[RouteController::class, 'index']);
+
+Route::redirect('/', '/routing');
+
+Route::get('/user/{id}', function($id) {
+    return "User Id: ".$id;
+});
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId) {
+    return "Post Id: ".$postId.", Comment Id: ".$commentId;
+});
+
+Route::get('username/{name?}', function($name = null) {
+    return 'Username: '.$name;
+});
+
+Route::get('/title/{title}', function($title) {
+    return "Title: ".$title;
+})->where('title', '[A-Za-z]+');
+
+Route::get('/profile/{profileId}', [RouteController::class, 'profile'])->name('profileRouteName');
